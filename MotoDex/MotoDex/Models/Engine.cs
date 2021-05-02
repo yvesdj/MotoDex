3b87cfbf1b06360eee7e155d8962e02678b1cbc4
yvesdj <yves.dejonghe@student.ap.be>
2021-05-02 14:25:06 +0200

    In process of making models

---
+++ MotoDex/MotoDex/Models/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotoDex.Models
+{
+    public class Engine
+    {
+        public string EngineType { get; set; }
+        public string EngineConfiguration { get; set; }
+        public float Capacity { get; set; }
+        public float MaxPower { get; set; }
+        public float MaxTorque { get; set; }
+        public string Cooling { get; set; }
+        public string InductionType { get; set; }
+        public string SparkPlug { get; set; }
+
+    }
+}
---
+++ MotoDex/MotoDex/Models/Motorcycle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotoDex.Models
+{
+    public class Motorcycle
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineType { get; set; }
+        public int Capacity { get; set; }
+        public string SparkPlug { get; set; }
+        public float Bhp { get; set; }
+        public string FrontTyre { get; set; }
+        public string RearTyre { get; set; }
+        public string FrontBreakPads { get; set; }
+        public string RearBreakPads { get; set; }
+    }
+}
