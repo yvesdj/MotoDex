585a5f1a7e48b6f8ef0dab70ac75e60d6756e424
yvesdj <yves.dejonghe@student.ap.be>
2021-05-03 00:52:05 +0200

    Added Additional data to test with

--- MotoDex/MotoDex/Db/DbInitializer.cs
+++ MotoDex/MotoDex/Db/DbInitializer.cs
@@ -16,13 +16,39 @@ namespace MotoDex.Db
             {
                 MotorcycleMake honda = new("HONDA", "Making motorcycles with the basic goal of bringing joy and satisfaction to people serves as the starting point of Honda. Honda will continue offering products which fulfill the needs of its customers around the world.");
 
-                Engine engine = new() { EngineConfiguration = "90 degree V4" };
+                Engine engine = new() { 
+                    EngineType = "4 Stroke",
+                    EngineConfiguration = "90 degree V4",
+                    Capacity = 748,
+                    MaxPower = 74,
+                    MaxTorque = 76.5f,
+                    Cooling = "Liquid cooled",
+                    InductionType = "Carburated",
+                    SparkPlug = "NGK CR8EH9"
+                };
 
                 Tyre fTyre = new() {
                     Make = "Pirelli",
+                    Model = "Angel GT2",
+                    TyreWidth = 120,
+                    HeightAspect = 70,
+                    RimSize = 17
                 };
 
-                BreakPad brkPad = new() { Make = "EBC" };
+                Tyre fTyre2 = new()
+                {
+                    Make = "Bridgestone",
+                    Model = "BattleAx Sport Touring T32",
+                    TyreWidth = 120,
+                    HeightAspect = 70,
+                    RimSize = 17
+                };
+
+                BreakPad brkPad = new() { 
+                    PadType = "Sintered",
+                    Make = "EBC",
+                    Model = "MCB598SV"
+                };
 
                 Motorcycle vfr = new()
                 {
@@ -31,7 +57,7 @@ namespace MotoDex.Db
                     Make = honda,
                     Engine = engine,
                     //RearTyre = new List<Tyre>() { fTyre },
-                    FrontTyre = new List<Tyre>() { fTyre },
+                    FrontTyre = new List<Tyre>() { fTyre, fTyre2 },
                     FrontBreakPads = brkPad,
                     RearBreakPads = brkPad
                 };
