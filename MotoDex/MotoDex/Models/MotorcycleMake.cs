5226ec9977d55e15364037d077946622b38af1b9
yvesdj <yves.dejonghe@student.ap.be>
2021-05-02 14:45:18 +0200

    A motorcycle has a Make, A make has different motorcycles

--- MotoDex/MotoDex/Models/MotorcycleMake.cs
+++ MotoDex/MotoDex/Models/MotorcycleMake.cs
@@ -1,6 +1,14 @@
-﻿namespace MotoDex.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotoDex.Models
 {
     public class MotorcycleMake
     {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+
+        public Motorcycle[] Motorcycles { get; set; }
     }
 }
