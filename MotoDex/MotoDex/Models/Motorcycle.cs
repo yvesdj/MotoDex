60b4a4335d5fc0069f880cf94f9add74e5d44134
yvesdj <yves.dejonghe@student.ap.be>
2021-05-04 13:15:46 +0200

    In process of making foreign keys accesable when creating new motorcycle

--- MotoDex/MotoDex/Models/Motorcycle.cs
+++ MotoDex/MotoDex/Models/Motorcycle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -11,14 +12,26 @@ namespace MotoDex.Models
     {
         [Key]
         public int Id { get; set; }
+
+        public int MakeId { get; set; }
         public MotorcycleMake Make { get; set; }
+
         public string Model { get; set; }
+
+        public int EngineId { get; set; }
         public Engine Engine { get; set; }
+
         public string FinalDrive { get; set; }
 
+        [ForeignKey("FrontTyreId")]
+        public ICollection<int> FrontTyreId { get; set; }
         public virtual ICollection<Tyre> FrontTyre { get; set; }
         //public virtual ICollection<Tyre> RearTyre { get; set; }
+
+        public int FrontBreakPadsId { get; set; }
         public BreakPad FrontBreakPads { get; set; }
+
+        public int RearBreakPadsId { get; set; }
         public BreakPad RearBreakPads { get; set; }
     }
 }
