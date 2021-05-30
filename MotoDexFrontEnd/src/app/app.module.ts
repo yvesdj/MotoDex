a0ab206900510bfd5b7235600ad5a2710af42b60
yvesdj <yves.dejonghe@student.ap.be>
2021-05-30 16:23:10 +0200

    Added routing module, home component

--- MotoDexFrontEnd/src/app/app.module.ts
+++ MotoDexFrontEnd/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
