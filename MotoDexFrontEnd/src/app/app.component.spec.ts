d3b18506812dfa135798e1c16df5d7be62b94540
yvesdj <yves.dejonghe@student.ap.be>
2021-05-30 16:15:32 +0200

    Made new Angular project with material

--- MotoDexFrontEnd/src/app/app.component.spec.ts
+++ MotoDexFrontEnd/src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MotoFrontEnd'`, () => {
+  it(`should have as title 'MotoDexFrontEnd'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MotoFrontEnd');
+    expect(app.title).toEqual('MotoDexFrontEnd');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MotoFrontEnd app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('MotoDexFrontEnd app is running!');
   });
 });
