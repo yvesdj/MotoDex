55e07ab7a0541f9ff9ca362392f2c2ca4885403d
yvesdj <yves.dejonghe@student.ap.be>
2021-05-31 10:11:22 +0200

    Added POST ability for Tyre, BreakPad

--- MotoDexFrontEnd/src/app/motorcycles/add-moto/add-moto.component.ts
+++ MotoDexFrontEnd/src/app/motorcycles/add-moto/add-moto.component.ts
@@ -26,6 +26,20 @@ export class AddMotoComponent implements OnInit {
     sparkPlug: ''
   })
 
+  tyreForm = this._formBuilder.group({
+    make: '',
+    model: '',
+    tyreWidth: '',
+    heightAspect: '',
+    rimSize: ''
+  })
+
+  breakPadForm = this._formBuilder.group({
+    padType: '',
+    make: '',
+    model: ''
+  })
+
   constructor(private _motoService: MotorcyclesService, private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
@@ -42,6 +56,20 @@ export class AddMotoComponent implements OnInit {
     let body = JSON.stringify(this.engineForm.value);
 
     this._motoService.PostObject(body, "engines")
+    this.engineForm.reset();
+  }
+
+  tyreOnSubmit(): void {
+    let body = JSON.stringify(this.engineForm.value);
+
+    this._motoService.PostObject(body, "tyres")
+    this.engineForm.reset();
+  }
+
+  breakPadOnSubmit(): void {
+    let body = JSON.stringify(this.engineForm.value);
+
+    this._motoService.PostObject(body, "breakpads")
     this.engineForm.reset();
   }
 
